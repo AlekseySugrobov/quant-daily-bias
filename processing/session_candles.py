64d624fea40e8ae6504f1899d804b7ca04526039
aleksey.sugrobov <sugrobov.al@esphere.ru>
2026-03-17 07:10:54 +0100

    refactor session candles

--- processing/session_candles.py
+++ processing/session_candles.py
@@ -1,6 +1,7 @@
 import pandas as pd
+import numpy as np
 
-def build_session_candles(df:pd.DataFrame) -> pd.DataFrame:
+def build_session_candles(df: pd.DataFrame) -> pd.DataFrame:
     sessions = df.groupby("session").agg(
         open=("open", "first"),
         high=("high", "max"),
@@ -11,8 +12,11 @@ def build_session_candles(df:pd.DataFrame) -> pd.DataFrame:
 
     sessions["range"] = sessions["high"] - sessions["low"]
 
-    sessions["type"] = "Doji"
-    sessions.loc[sessions["close"] < sessions["open"], "type"] = "Bear"
-    sessions.loc[sessions["close"] > sessions["open"], "type"] = "Bull"
+    conditions = [
+        sessions["close"] < sessions["open"],
+        sessions["close"] > sessions["open"]
+    ]
+
+    sessions["type"] = np.select(conditions, ["Bear", "Bull"], default="Doji")
 
     return sessions.reset_index()
